0ac89608879e86e778edf138250ecb77ff3bdbfe
kushal bhargava <kushal.git01@gmail.com>
2025-06-18 00:01:44 +0530

    add Linked List Cycle

---
+++ Linked List/Linked List Cycle.js
@@ -0,0 +1,52 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function (head) {
+  let seenNode = new Set();
+  let curr = head;
+  while (curr) {
+    if (seenNode.has(curr)) {
+      return true;
+    }
+    seenNode.add(curr);
+    curr = curr.next;
+  }
+  return false;
+};
+
+// Approach 2 flyod's cycle detection algorithm
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function (head) {
+  let slow = head;
+  let fast = head;
+
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+
+    if (slow === fast) return true;
+  }
+
+  return false;
+};
