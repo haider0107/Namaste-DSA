8908afd8482dc2449a12ac9a0e496ea3c81fa65e
kushal bhargava <kushal.git01@gmail.com>
2025-07-19 22:40:09 +0530

    update Find Minimum in Rotated Sorted Array

--- Binary Search Algorithem/Find Minimum in Rotated Sorted Array.js
+++ Binary Search Algorithem/Find Minimum in Rotated Sorted Array.js
@@ -3,19 +3,44 @@
  * @return {number}
  */
 var findMin = function (nums) {
+  /*
+    
+    let l = 0
+    let r = nums.length -1
+
+    while(l<r){
+        let m = l + Math.floor((r-l)/2)
+
+        if(nums[m] > nums[l] && nums[l] < nums[r]){
+            r = m-1
+        }else{
+            if(nums[m] < nums[r]) r = m
+            else l = m+1
+        }
+    }
+
+    return nums[l]
+    
+    */
+
   let l = 0;
   let r = nums.length - 1;
 
-  while (l < r) {
-    let m = l + Math.floor((r - l) / 2);
-
-    if (nums[m] > nums[l] && nums[l] < nums[r]) {
-      r = m - 1;
-    } else {
-      if (nums[m] < nums[r]) r = m;
-      else l = m + 1;
-    }
+  if (nums[l] <= nums[r]) {
+    return nums[l];
   }
 
-  return nums[l];
+  while (l <= r) {
+    let m = l + Math.floor((r - l) / 2);
+
+    if (nums[l] <= nums[r]) {
+      return nums[l];
+    } else if (nums[m] < nums[m - 1]) {
+      return nums[m];
+    } else if (nums[l] > nums[m]) {
+      r = m - 1;
+    } else {
+      l = m + 1;
+    }
+  }
 };
