c45abea0c31f8956ab862099ee13fd35578af892
kushal bhargava <kushal.git01@gmail.com>
2025-07-29 18:03:05 +0530

    add Intersection of Two Linked Lists(with two pointer)

---
+++ Two Pointer & Sliding Window/Intersection of Two Linked Lists(with two pointer).js
@@ -0,0 +1,67 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+var getIntersectionNode = function (headA, headB) {
+  /*
+    
+    let store = new Set()
+
+    while(headA){
+        store.add(headA)
+        headA = headA.next
+    }
+
+    while(headB){
+        if(store.has(headB)) return headB
+
+        headB = headB.next
+    }
+
+    return null
+    */
+
+  let lenA = 0;
+  let lenB = 0;
+
+  let curr = headA;
+
+  while (curr) {
+    lenA++;
+    curr = curr.next;
+  }
+  curr = headB;
+  while (curr) {
+    lenB++;
+    curr = curr.next;
+  }
+
+  if (lenA > lenB) {
+    let diff = lenA - lenB;
+    for (let i = 0; i < diff; i++) {
+      headA = headA.next;
+    }
+  } else {
+    let diff = lenB - lenA;
+    for (let i = 0; i < diff; i++) {
+      headB = headB.next;
+    }
+  }
+
+  while (headA) {
+    if (headA === headB) return headA;
+    headA = headA.next;
+    headB = headB.next;
+  }
+
+  return null;
+};
