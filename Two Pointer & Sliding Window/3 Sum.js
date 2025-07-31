e09bb6194f46b014298d64d9970ed8d10104270a
kushal bhargava <kushal.git01@gmail.com>
2025-08-01 02:35:41 +0530

    update 3 Sum

--- Two Pointer & Sliding Window/3 Sum.js
+++ Two Pointer & Sliding Window/3 Sum.js
@@ -3,33 +3,31 @@
  * @return {number[][]}
  */
 
-var threeSum = function (nums) {
-  nums.sort((a, b) => a - b);
-  let ans = [];
-  for (let i = 0; i < nums.length; i++) {
-    if (i === 0 || nums[i] !== nums[i - 1]) {
-      twoSum(nums, i, ans);
-    }
-  }
-  return ans;
-};
-var twoSum = function (arr, x, ans) {
-  let i = x + 1;
-  let j = arr.length - 1;
-  while (i < j) {
-    let sum = arr[i] + arr[j] + arr[x];
-    if (sum > 0) {
-      j--;
-    } else if (sum < 0) {
-      i++;
-    } else {
-      ans.push([arr[i], arr[j], arr[x]]);
-      i++;
-      j--;
-      // Skip duplicates for the second element
-      while (i < j && arr[i] === arr[i - 1]) {
-        i++;
+var twoSum = function (x, l, r, nums, res) {
+  while (l < r) {
+    let sum = nums[l] + nums[r] + nums[x];
+
+    if (sum === 0) {
+      res.push([nums[l], nums[r], nums[x]]);
+      l++;
+      r--;
+      while (nums[l] === nums[l - 1]) {
+        l++;
       }
+    } else if (sum > 0) r -= 1;
+    else l += 1;
+  }
+};
+
+var threeSum = function (nums) {
+  let sortArr = nums.sort((a, b) => a - b);
+
+  let res = [];
+  for (let i = 0; i < sortArr.length - 2; i++) {
+    if (sortArr[i] !== sortArr[i - 1]) {
+      twoSum(i, i + 1, sortArr.length - 1, sortArr, res);
     }
   }
+
+  return res;
 };
