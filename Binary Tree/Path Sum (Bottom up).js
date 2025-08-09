9755b291c82f880ee2a7c146ba18b155ebd4c5e2
kushal bhargava <kushal.git01@gmail.com>
2025-08-09 22:55:03 +0530

    add Path Sum

---
+++ Binary Tree/Path Sum (Bottom up).js
@@ -0,0 +1,53 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @return {boolean}
+ */
+var hasPathSum = function (root, targetSum) {
+  // let flag = false
+
+  // if (!root) return false
+
+  // function traversal(node, sum) {
+  //         if (!node){
+  //             return
+  //         }
+
+  //         if (flag) return
+
+  //         sum += node.val
+
+  //         if (targetSum === sum) {
+  //             if (!node.left && !node.right) {
+  //                 flag = true
+  //             }
+
+  //         }
+
+  //         traversal(node.left, sum)
+  //         traversal(node.right, sum)
+  //     }
+
+  //     traversal(root, 0)
+
+  //     return flag
+
+  if (!root) return false;
+
+  if (!root.left && !root.right) {
+    if (root.val === targetSum) return true;
+  }
+
+  let left = hasPathSum(root.left, targetSum - root.val);
+  let right = hasPathSum(root.right, targetSum - root.val);
+
+  return left || right;
+};
