f6323d37a0caf479cc08a87f3e07f4ab41fbaf0d
kushal bhargava <kushal.git01@gmail.com>
2025-10-10 00:02:47 +0530

    update Coin change

---
+++ Dynamic Programming/Coin Change/bottom up.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} coins
+ * @param {number} amount
+ * @return {number}
+ */
+var coinChange = function (coins, amount) {
+  let n = coins.length;
+  let dp = new Array(amount + 1).fill(Infinity);
+
+  dp[0] = 0;
+
+  for (let i = 1; i <= amount; i++) {
+    for (let j = 0; j < n; j++) {
+      let remAmt = i - coins[j];
+      if (remAmt >= 0) {
+        dp[i] = Math.min(dp[i], 1 + dp[remAmt]);
+      }
+    }
+  }
+
+  return dp[amount] === Infinity ? -1 : dp[amount];
+};
