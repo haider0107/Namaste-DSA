bbeba0b54005dcc2a01680421359091d140aaaa1
kushal bhargava <kushal.git01@gmail.com>
2025-10-17 23:18:00 +0530

    add Maximum Product Subarray

---
+++ Dynamic Programming/Maximum Product Subarray/index.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxProduct = function (nums) {
+  let maxProd = (minProd = nums[0]);
+
+  let max = nums[0];
+
+  for (let i = 1; i < nums.length; i++) {
+    let temp = maxProd;
+
+    maxProd = Math.max(nums[i], maxProd * nums[i], minProd * nums[i]);
+    minProd = Math.min(nums[i], temp * nums[i], minProd * nums[i]);
+
+    max = Math.max(max, maxProd);
+  }
+
+  return max;
+};
