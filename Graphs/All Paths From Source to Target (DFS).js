fb59d68cb28b17899fc4a6506bc367b2ef160c2d
kushal bhargava <kushal.git01@gmail.com>
2025-11-30 19:40:39 +0530

    add All Paths From Source to Target (DFS)

---
+++ Graphs/All Paths From Source to Target (DFS).js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[][]} graph
+ * @return {number[][]}
+ */
+var allPathsSourceTarget = function (graph) {
+  const res = [];
+
+  const dfs = (path) => {
+    const node = path[path.length - 1];
+    if (node === graph.length - 1) {
+      res.push([...path]);
+      return;
+    }
+
+    for (const n of graph[node]) {
+      path.push(n);
+
+      dfs(path);
+
+      path.pop();
+    }
+  };
+
+  dfs([0], 0);
+
+  return res;
+};
